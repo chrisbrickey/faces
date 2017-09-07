94a4e8106aec9db705c983461d1f09b3bad80f7c
chrisbrickey <cbrickey1@gmail.com>
2017-09-07 12:37:26 -0700

    add upload/display capability using cloudinary, functional at this point

--- faces2/src/upload.jsx
+++ faces2/src/upload.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Dropzone from 'react-dropzone';
 import request from 'superagent';
 
+
+
+const CLOUDINARY_UPLOAD_PRESET = 'swicrc8u';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/dckkkjkuz/image/upload';
+
 class Upload extends React.Component {
   constructor(props) {
     super(props);
@@ -11,12 +16,30 @@ class Upload extends React.Component {
       },
     };
 
+    window.state = this.state;
     this.onImageDrop = this.onImageDrop.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   onImageDrop(files) {
+    this.handleImageUpload(files[0]);
+  }
 
+  handleImageUpload(file) {
+    let upload = request.post(CLOUDINARY_UPLOAD_URL)
+                          .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                          .field('file', file);
+
+      upload.end((err, response) => {
+        if (err) {
+          console.error(err);
+        }
+
+        if (response.body.secure_url !== 'z') {
+          this.setState({ photo: { photo_url: response.body.secure_url}});
+        }
+
+      });
   }
 
   handleSubmit(event) {
